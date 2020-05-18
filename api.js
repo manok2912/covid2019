70c95e041592e0a47e3db616799c2ba3e3118fae
manoj <manojkumarch2020@gmail.com>
2020-05-18 14:39:56 +0530

    new update
     added style.css
    and some major changes in UI

--- api.js
+++ api.js
@@ -1,16 +1,23 @@
 var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-var myJSON
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+}
 $(document).ready(function(){
+
     
     $.getJSON("https://covid19.mathdro.id/api", function(result){
         //console.log(result);
-        $("#confirmed").text(result.confirmed.value);
-        $("#recovered").text(result.recovered.value);
-        $("#deaths").text(result.deaths.value);
+        $("#confirmed").text(numberWithCommas(result.confirmed.value));
+        $("#recovered").text(numberWithCommas(result.recovered.value));
+        $("#deaths").text(numberWithCommas(result.deaths.value));
+        var active = result.confirmed.value - (result.recovered.value + result.deaths.value);
+        $("#active").text(numberWithCommas(active));
+        var ap = active / result.confirmed.value * 100;
+        $("#activePercentage").text(ap.toFixed(1)+"% affected rate");
         var rp = result.recovered.value / result.confirmed.value * 100;
         $("#recoveredPercentage").text(rp.toFixed(1)+"% recovery rate");
         var dp = result.deaths.value / result.confirmed.value * 100;
-        $("#deathsPercentage").text(dp.toFixed(2)+"% fatality rate");
+        $("#deathsPercentage").text(dp.toFixed(1)+"% fatality rate");
         var d = new Date()
         var str = result.lastUpdate;
         var arr = str.split("T");
@@ -25,12 +32,14 @@ $(document).ready(function(){
         $.getJSON( url, function(res){
             $("#CountryStat").find("#"+res.country_code_iso3).children().css("background-color", "yellow");
             var ipurl = "https://covid19.mathdro.id/api/countries/"+res.country_code_iso3;
+            var curlimg = "https://www.countryflags.io/"+res.country+"/shiny/48.png"
+            $("#cflag").attr("src",curlimg);
             $.getJSON( ipurl, function(r){
                 $("#ipcountryName").text(res.country_name);
-                $("#ipcountryConfirmed").text(r.confirmed.value);
-                $("#ipcountryRecovered").text(r.recovered.value);
-                $("#ipcountryDeaths").text(r.deaths.value);
-                $("#ipcountryActive").text(r.confirmed.value - (r.recovered.value+r.deaths.value));
+                $("#ipcountryConfirmed").text(numberWithCommas(r.confirmed.value));
+                $("#ipcountryRecovered").text(numberWithCommas(r.recovered.value));
+                $("#ipcountryDeaths").text(numberWithCommas(r.deaths.value));
+                $("#ipcountryActive").text(numberWithCommas(r.confirmed.value - (r.recovered.value+r.deaths.value)));
                 var iprecovery = r.recovered.value/r.confirmed.value * 100;
                 var ipdeath = r.deaths.value/r.confirmed.value * 100;
                 var active = r.confirmed.value - (r.recovered.value+r.deaths.value)
@@ -38,42 +47,39 @@ $(document).ready(function(){
                 $("#ipcountryRecoveredper").text(iprecovery.toFixed(2)+"% recovery rate");
                 $("#ipcountryDeathspre").text(ipdeath.toFixed(2)+"% fatality rate");
                 $("#ipcountryActivepre").text(ipactive.toFixed(2)+"% affected rate");
-                console.log(r.confirmed.value);
-                console.log(r.recovered.value);
-                console.log(r.deaths.value);
-                console.log(r.confirmed.value/res.country_population);
-                var manoj = res.country_population
-                console.log(manoj.length);
                 console.log(res.country_name);
-                console.log(res.country_capital);
-                var total = $("#confirmed").text();
-                console.log(total);
+                var total = $("#confirmed").text().replace(/,/g,"");
                 var ippre = r.confirmed.value/total * 100;
-                console.log(ippre);
                 var ippre = ippre.toFixed(2);
-                console.log(ippre);
                 $("#ipcountryConfirmedpre").text(ippre+"% across global");
-                
-                
             });  
             });
-        });
+    });
     
-  
-   $.getJSON( "https://covid19.mathdro.id/api/daily", function( data ) {
+
+    $.getJSON("https://covid19.mathdro.id/api/countries",function(data){
+        $.each(data , function(key , val){
+            val.forEach(function(item){
+                if (item.hasOwnProperty('iso3')){
+                    var url = "https://covid19.mathdro.id/api/countries/"+item.iso3
+                    $.getJSON(url , function(data){
+                        var activecountry = data.confirmed.value - (data.recovered.value + data.deaths.value);
+                        $("#CountryStat").append("<tr id="+item.iso3+"><td class='text-center'>"+item.name+"</td><td class='text-center text-danger backred'>"+numberWithCommas(data.confirmed.value)+"</td><td class='text-center text-primary backblue'>"+numberWithCommas(activecountry)+"</td><td class='text-center text-success backgreen'>"+numberWithCommas(data.recovered.value)+"</td><td class='text-center text-secondary backgray'>"+numberWithCommas(data.deaths.value)+"</td></tr>");
+                    });
+                }
+            });
+        });
+
+    $.getJSON( "https://covid19.mathdro.id/api/daily", function( data ) {
        $.each( data, function(key, val) {
            var d = new Date(val.reportDate);
-           $("#dailyStat").append("<tr><td>"+months[d.getMonth()]+" "+d.getDate()+"</td><td>"+val.mainlandChina+"</td><td>"+val.otherLocations+"</td><td>"+val.totalConfirmed+"</td><td>"+val.deltaConfirmed+"</td><td>"+val.totalRecovered+"</td><td>"+val.deltaRecovered+"</td></tr>");
+           $("#dailyStat").append("<tr><td class='text-center'>"+months[d.getMonth()]+" "+d.getDate()+"</td><td class='text-center text-danger backred'>"+numberWithCommas(val.otherLocations)+" <span class='deltaconfrim'>&#x25B2;"+numberWithCommas(val.deltaConfirmedDetail.outsideChina)+"</span></td><td class='text-center text-danger backred'>"+numberWithCommas(val.mainlandChina)+" <span class='deltaconfrim'>&#x25B2;"+numberWithCommas(val.deltaConfirmedDetail.china)+"</span></td><td class='text-center text-danger backred'>"+numberWithCommas(val.totalConfirmed)+" <span class='deltaconfrim'>&#x25B2;"+numberWithCommas(val.deltaConfirmedDetail.total)+"</span></td><td class='text-center text-secondary backgray'>"+numberWithCommas(val.deaths.outsideChina)+"</td><td class='text-center text-secondary backgray'>"+numberWithCommas(val.deaths.china)+"</td><td class='text-center text-secondary backgray'>"+numberWithCommas(val.deaths.total)+"</td></tr>");
            var cp = val.otherLocations/val.totalConfirmed * 100;
            $("#confirmedPercentage").text("outside china "+cp.toFixed(1)+"%");
-       })
-   });
-    
-    $.getJSON( "https://covid19.mathdro.id/api/confirmed", function( data ){
-         $.each( data, function(key, val) {
-             state = (val.provinceState == null) ? "-" : val.provinceState;
-             $("#CountryStat").append("<tr id="+val.iso3+" ><td>"+val.countryRegion+"</td><td>"+state+"</td><td>"+val.confirmed+"</td><td>"+val.recovered+"</td><td>"+val.deaths+"</td><td>"+val.active+"</td></tr>");  
-         });     
+       });
+    });
+
+
     });
     
     
